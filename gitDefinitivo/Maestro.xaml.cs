d16788d23ead5cefb629c73d1a79edef7aa161a9
Lizethmercado <lizeth.mercadob@cecar.edu.co>
2024-10-14 22:32:18 -0500

    agregué una nueva pagina (favor probar la rama)

--- gitDefinitivo/Maestro.xaml.cs
+++ gitDefinitivo/Maestro.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using gitDefinitivo.Pages;
 using Microsoft.Maui.Controls;
 
 namespace gitDefinitivo;
@@ -23,6 +24,11 @@ namespace gitDefinitivo;
         App.FlyoutPage.IsPresented = false;
     }
 
+
+    public static void presiona()
+    {
+        NavigationToPage(new Prueba());
+    }
 
 
 }
